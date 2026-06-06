b48e8c64a5efad6d02af5b6ce5a0c7c863573dd4
Roman_Lubennikov <lubennikovroman@gmail.com>
2026-06-06 10:45:50 +0300

    03-valid

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -2,8 +2,49 @@ import { Note } from '../models/note.js';
 import createHttpError from 'http-errors';
 
 export const getAllNotes = async (req, res) => {
-  const notes = await Note.find();
-  res.status(200).json(notes);
+  const { page = 1, perPage = 10, tag, search } = req.query;
+
+  const pageNumber = Number(page);
+  const perPageNumber = Number(perPage);
+
+  const filter = {};
+
+  if (tag) {
+    filter.tag = tag;
+  }
+
+  if (search) {
+    filter.$or = [
+      {
+        title: {
+          $regex: search,
+          $options: 'i',
+        },
+      },
+      {
+        content: {
+          $regex: search,
+          $options: 'i',
+        },
+      },
+    ];
+  }
+
+  const totalNotes = await Note.countDocuments(filter);
+
+  const notes = await Note.find(filter)
+    .skip((pageNumber - 1) * perPageNumber)
+    .limit(perPageNumber);
+
+  const totalPages = Math.ceil(totalNotes / perPageNumber);
+
+  res.status(200).json({
+    page: pageNumber,
+    perPage: perPageNumber,
+    totalNotes,
+    totalPages,
+    notes,
+  });
 };
 
 export const getNoteById = async (req, res) => {
--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import { errors } from 'celebrate';
 import { connectMongoDB } from './db/connectMongoDB.js';
 import { logger } from './middleware/logger.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
@@ -14,6 +15,7 @@ app.use(cors());
 app.use(express.json());
 app.use(notesRouter);
 const PORT = process.env.PORT || 3000;
+app.use(errors());
 app.use(notFoundHandler);
 app.use(errorHandler);
 await connectMongoDB();
