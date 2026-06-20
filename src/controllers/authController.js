2a7dcd0fa8346518c673da7ec05d0c70114a6adc
Roman_Lubennikov <lubennikovroman@gmail.com>
2026-06-20 12:47:12 +0300

    05

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -5,7 +5,8 @@ import { User } from '../models/user.js';
 import { Session } from '../models/session.js';
 
 import { createSession, setSessionCookies } from '../services/auth.js';
-
+import jwt from 'jsonwebtoken';
+import { sendEmail } from '../utils/sendMail.js';
 export const registerUser = async (req, res, next) => {
   try {
     const { email, password } = req.body;
@@ -114,4 +115,69 @@ export const logoutUser = async (req, res, next) => {
   } catch (err) {
     next(err);
   }
+};
+export const requestResetEmail = async (req, res) => {
+  const { email } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    return res.status(200).json({
+      message: 'Password reset email sent successfully',
+    });
+  }
+
+  const resetToken = jwt.sign(
+    {
+      sub: user._id,
+      email: user.email,
+    },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: '15m',
+    },
+  );
+
+  const resetLink = `${process.env.FRONTEND_DOMAIN}/reset-password?token=${resetToken}`;
+
+  await sendEmail({
+    from: process.env.SMTP_FROM,
+    to: user.email,
+    subject: 'Reset password',
+    html: `<a href="${resetLink}">Reset password</a>`,
+  });
+
+  res.status(200).json({
+    message: 'Password reset email sent successfully',
+  });
+};
+export const resetPassword = async (req, res) => {
+  const { token, password } = req.body;
+
+  let payload;
+
+  try {
+    payload = jwt.verify(token, process.env.JWT_SECRET);
+  } catch {
+    throw createHttpError(401, 'Invalid or expired token');
+  }
+
+  const user = await User.findOne({
+    _id: payload.sub,
+    email: payload.email,
+  });
+
+  if (!user) {
+    throw createHttpError(404, 'User not found');
+  }
+
+  const hashPassword = await bcrypt.hash(password, 10);
+
+  user.password = hashPassword;
+
+  await user.save();
+
+  res.status(200).json({
+    message: 'Password reset successfully',
+  });
 };
--- src/server.js
+++ src/server.js
@@ -3,25 +3,37 @@ import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv';
 import { errors } from 'celebrate';
+
 import { connectMongoDB } from './db/connectMongoDB.js';
+
 import { logger } from './middleware/logger.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
+
 import notesRouter from './routes/notesRoutes.js';
 import authRouter from './routes/authRoutes.js';
+import userRouter from './routes/userRoutes.js';
+
 dotenv.config();
 
 const app = express();
+
 app.use(logger);
 app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
+
 app.use(authRouter);
 app.use(notesRouter);
-const PORT = process.env.PORT || 3000;
+app.use('/users', userRouter);
+
 app.use(errors());
+
 app.use(notFoundHandler);
 app.use(errorHandler);
+
+const PORT = process.env.PORT || 3000;
+
 await connectMongoDB();
 
 app.listen(PORT, () => {
